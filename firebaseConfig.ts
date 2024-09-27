19913948545ec3418a1600d30125383aac89fa99
vivy_phtela <tubasa.0405@icloud.com>
2024-09-28 00:59:09 +0900

    add: app checkの導入

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -2,6 +2,11 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import {
+  initializeAppCheck,
+  ReCaptchaV3Provider,
+  getToken,
+} from "firebase/app-check";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_APIKEY,
@@ -18,3 +23,30 @@ const db = getFirestore(app);
 const storage = getStorage(app);
 
 export { auth, db, storage };
+
+// FIREBASE_APPCHECK_DEBUG_TOKENの型定義
+declare global {
+  var FIREBASE_APPCHECK_DEBUG_TOKEN: boolean | string | undefined;
+}
+
+if (typeof document !== "undefined") {
+  // 1.デバック環境用設定
+  if (process.env.NODE_ENV === "development") {
+    window.self.FIREBASE_APPCHECK_DEBUG_TOKEN = true; // デバッグ用文字列の生成
+  }
+  // 2.AppCheck 初期化
+  const appCheck = initializeAppCheck(app, {
+    provider: new ReCaptchaV3Provider(
+      process.env.NEXT_PUBLIC__RECAPTCHA_SITEKEY as string
+    ),
+    isTokenAutoRefreshEnabled: true,
+  });
+  // 3.AppCheck結果 ＆ トークン確認
+  getToken(appCheck)
+    .then(() => {
+      console.log("AppCheck:Success");
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
+}
